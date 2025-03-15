38214b2229b69686275309bd445dd8b915fdc994
Andrii Torholskyi <torgolskijandrej@gmail.com>
2025-03-15 16:28:02 +0200

    Adding files to controller package

--- src/main/java/ntu/dp/torholskyi/Run.java
+++ src/main/java/ntu/dp/torholskyi/Run.java
@@ -1,6 +1,6 @@
 package ntu.dp.torholskyi;
 
-public class Main {
+public class Run {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
---
+++ src/main/java/ntu/dp/torholskyi/controller/StudentCreator.java
@@ -0,0 +1,4 @@
+package ntu.dp.torholskyi.controller;
+
+public class StudentCreator {
+}
