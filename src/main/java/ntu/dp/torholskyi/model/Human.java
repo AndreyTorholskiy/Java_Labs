ec7000f41f3d7da4f87ad32418df1a2de3fddd6f
AndriiTorholskyi <Torholskyi.An.O@nmu.one>
2025-03-18 14:27:51 +0200

    Add student that extends human and adding lombok to dependence

--- src/main/java/ntu/dp/torholskyi/model/Human.java
+++ src/main/java/ntu/dp/torholskyi/model/Human.java
@@ -1,7 +1,10 @@
 package ntu.dp.torholskyi.model;
 
+import lombok.Data;
+
 import java.sql.Date;
 
+@Data
 public abstract class Human {
     private long id;
     private String firstName;
@@ -9,7 +12,22 @@ public abstract class Human {
     private String lastName;
     private Date birthDate;
 
+    public Human(long id, String firstName, String middleName, String lastName, Date birthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
 
-
-
+    @Override
+    public String toString() {
+        return "Human{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
