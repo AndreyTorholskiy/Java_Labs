794f850bf84de09863bc2fe21efeb630782e7541
AndriiTorholskyi <Torholskyi.An.O@nmu.one>
2025-03-18 14:17:12 +0200

    Add abstract human class

---
+++ src/main/java/ntu/dp/torholskyi/model/Human.java
@@ -0,0 +1,15 @@
+package ntu.dp.torholskyi.model;
+
+import java.sql.Date;
+
+public abstract class Human {
+    private long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private Date birthDate;
+
+
+
+
+}
